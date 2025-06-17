0b81249fe9b0445f8ba8bf8af6fe2ed8326ef298
Nasir-afridi <nasirwc05@gmail.com>
2025-06-17 16:47:48 +0500

    build the simple ui of basic calculator

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+function App() {
+  return (
+    <>
+      <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+        <div className="card shadow p-4" style={{ width: "320px" }}>
+          <input
+            type="text"
+            className="form-control text-end fs-4 mb-3"
+            placeholder="0"
+            readOnly
+          />
+
+          <div className="row g-2">
+            <div className="col-3"><button className="btn btn-primary w-100">7</button></div>
+            <div className="col-3"><button className="btn btn-primary w-100">8</button></div>
+            <div className="col-3"><button className="btn btn-primary w-100">9</button></div>
+            <div className="col-3"><button className="btn btn-warning w-100">/</button></div>
+
+            <div className="col-3"><button className="btn btn-primary w-100">4</button></div>
+            <div className="col-3"><button className="btn btn-primary w-100">5</button></div>
+            <div className="col-3"><button className="btn btn-primary w-100">6</button></div>
+            <div className="col-3"><button className="btn btn-warning w-100">*</button></div>
+
+            <div className="col-3"><button className="btn btn-primary w-100">1</button></div>
+            <div className="col-3"><button className="btn btn-primary w-100">2</button></div>
+            <div className="col-3"><button className="btn btn-primary w-100">3</button></div>
+            <div className="col-3"><button className="btn btn-warning w-100">-</button></div>
+
+            <div className="col-3"><button className="btn btn-primary w-100">0</button></div>
+            <div className="col-3"><button className="btn btn-danger w-100">C</button></div>
+            <div className="col-3"><button className="btn btn-success w-100">=</button></div>
+            <div className="col-3"><button className="btn btn-warning w-100">+</button></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default App;
